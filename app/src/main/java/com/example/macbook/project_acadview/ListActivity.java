3493cb781f80598178f08c74ed773ea925e5008e
Pankaj <pankajdevrath15@gmail.com>
2018-06-28 15:27:58 +0530

    commit

--- app/src/main/java/com/example/macbook/project_acadview/ListActivity.java
+++ app/src/main/java/com/example/macbook/project_acadview/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.macbook.project_acadview;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.*;
 import android.widget.ListView;
@@ -27,10 +28,47 @@ public class ListActivity extends AppCompatActivity {
          list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(),showToast.class);
+
+                final String text = (String) parent.getItemAtPosition(position);
+                if ("dialerpad".compareTo(text) == 0) {
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("tel:9728080928"));
+                    startActivity(intent);
+
+                }
+                else if ("browser".compareTo(text) == 0) {
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://www.google.com"));
+                    startActivity(intent);
+
+                }
+                else if ("camera".compareTo(text) == 0) {
+
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1888);
+
+                }
+                else if ("map".compareTo(text) == 0) {
+
+                    Uri location = Uri.parse("geo:29.149188, 75.721653");
+                    Intent i = new Intent(Intent.ACTION_VIEW, location);
+                    startActivity(i);
+
+                }
+                else if ("gmail".compareTo(text) == 0) {
+
+                    Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
+                    startActivity(intent);
+
+                } else {
+
+
+                    Intent intent = new Intent(getApplicationContext(),showToast.class);
                 intent.putExtra("name", arrayList.get(position));
                 startActivity(intent);
-            }
+            }}
         });
 
 
--- app/src/main/java/com/example/macbook/project_acadview/MainActivity.java
+++ app/src/main/java/com/example/macbook/project_acadview/MainActivity.java
@@ -25,44 +25,44 @@ public class MainActivity extends AppCompatActivity {
     public void click(View v) {
         EditText text = findViewById(R.id.editText);
         myList.add(text.getText().toString());
-        if ("dialerpad".compareTo(text.getText().toString()) == 0) {
-
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("tel:9728080928"));
-            startActivity(intent);
-
-        }
-        else if ("browser".compareTo(text.getText().toString()) == 0) {
-
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("http://www.google.com"));
-            startActivity(intent);
-
-        }
-        else if ("camera".compareTo(text.getText().toString()) == 0) {
-
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, 1888);
-
-        }
-        else if ("map".compareTo(text.getText().toString()) == 0) {
-
-            Uri location = Uri.parse("geo:29.149188, 75.721653");
-            Intent i = new Intent(Intent.ACTION_VIEW, location);
-            startActivity(i);
-
-        }
-        else if ("gmail".compareTo(text.getText().toString()) == 0) {
-
-            Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
-            startActivity(intent);
-
-        } else {
+//        if ("dialerpad".compareTo(text.getText().toString()) == 0) {
+//
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setData(Uri.parse("tel:9728080928"));
+//            startActivity(intent);
+//
+//        }
+//        else if ("browser".compareTo(text.getText().toString()) == 0) {
+//
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setData(Uri.parse("http://www.google.com"));
+//            startActivity(intent);
+//
+//        }
+//        else if ("camera".compareTo(text.getText().toString()) == 0) {
+//
+//            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//            startActivityForResult(cameraIntent, 1888);
+//
+//        }
+//        else if ("map".compareTo(text.getText().toString()) == 0) {
+//
+//            Uri location = Uri.parse("geo:29.149188, 75.721653");
+//            Intent i = new Intent(Intent.ACTION_VIEW, location);
+//            startActivity(i);
+//
+//        }
+//        else if ("gmail".compareTo(text.getText().toString()) == 0) {
+//
+//            Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
+//            startActivity(intent);
+//
+//        } else {
             Intent intent = new Intent(this, ListActivity.class);
             intent.putExtra("list_passed", myList);
 
             startActivity(intent);
 
-    }
+
     }
 }
